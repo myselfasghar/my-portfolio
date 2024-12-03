899718e6b22333d50af40e17f00bc05a2471c34d
Aditya chandel <adityachandel371@gmail.com>
2024-12-03 19:45:34 +0530

    Added animation through scroll reveal

--- script.js
+++ script.js
@@ -3,3 +3,38 @@ document.querySelector('.menu-btn').addEventListener('click', function () {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('active');
 });
+
+// Animation
+
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: false
+});
+
+// home section
+sr.reveal('.main-content', { delay: 200, origin: 'top' });
+sr.reveal('.content-image', { delay: 200, origin: 'top' });
+
+// about section
+sr.reveal('.title', { delay: 200, origin: 'top' });
+sr.reveal('.box-wrap', { delay: 200, origin: 'left' });
+sr.reveal('.box-wrap', { delay: 200, origin: 'left' });
+sr.reveal('.box-wrap', { delay: 200, origin: 'left' });
+
+// skills section
+sr.reveal('.topic1', { delay: 200, origin: 'right' });
+sr.reveal('.tool', { delay: 200, origin: 'left' });
+sr.reveal('.topic2', { delay: 200, origin: 'right' });
+sr.reveal('.tech', { delay: 200, origin: 'left' });
+
+// projects section
+sr.reveal('.project-box-wrap', { delay: 200, origin: 'left' });
+
+// contact section
+sr.reveal('.container', { delay: 200, origin: 'left' });
+
+// footer
+sr.reveal('.footer-text', { delay: 200, origin: 'left' });
+sr.reveal('.icon', { delay: 200, origin: 'right' });
