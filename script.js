ec7f1577cb33fd1f6b11f434dcc99eb0486c30ec
Aditya chandel <adityachandel371@gmail.com>
2024-12-04 10:47:16 +0530

    Fixed something

--- script.js
+++ script.js
@@ -8,29 +8,29 @@ document.querySelector('.menu-btn').addEventListener('click', function () {
 
 const sr = ScrollReveal({
     distance: '65px',
-    duration: 2600,
-    delay: 450,
+    duration: 2000,
+    delay: 250,
     reset: false
 });
 
 // home section
-sr.reveal('.main-content', { delay: 200, origin: 'top' });
-sr.reveal('.content-image', { delay: 200, origin: 'top' });
+sr.reveal('.main-content', { delay: 100, origin: 'top' });
+sr.reveal('.content-image', { delay: 100, origin: 'top' });
 
 // about section
-sr.reveal('.title', { delay: 200, origin: 'top' });
-sr.reveal('.box-wrap', { delay: 200, origin: 'left' });
-sr.reveal('.box-wrap', { delay: 200, origin: 'left' });
-sr.reveal('.box-wrap', { delay: 200, origin: 'left' });
+sr.reveal('.title', { delay: 100, origin: 'top' });
+sr.reveal('.box-wrap', { delay: 100, origin: 'left' });
+sr.reveal('.box-wrap', { delay: 100, origin: 'left' });
+sr.reveal('.box-wrap', { delay: 100, origin: 'left' });
 
 // skills section
-sr.reveal('.topic1', { delay: 200, origin: 'right' });
-sr.reveal('.tool', { delay: 200, origin: 'left' });
-sr.reveal('.topic2', { delay: 200, origin: 'right' });
-sr.reveal('.tech', { delay: 200, origin: 'left' });
+sr.reveal('.topic1', { delay: 100, origin: 'right' });
+sr.reveal('.tool', { delay: 100, origin: 'left' });
+sr.reveal('.topic2', { delay: 100, origin: 'right' });
+sr.reveal('.tech', { delay: 100, origin: 'left' });
 
 // projects section
-sr.reveal('.project-box-wrap', { delay: 200, origin: 'left' });
+sr.reveal('.project-box-wrap', { delay: 100, origin: 'left' });
 
 // contact section
-sr.reveal('.container', { delay: 200, origin: 'left' });
+sr.reveal('.container', { delay: 100, origin: 'left' });
