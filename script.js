634ef1d7213eebf908d786b3eece2e6aa693d052
Aditya chandel <adityachandel371@gmail.com>
2025-02-07 19:55:30 +0530

    Added js logic to switch between menu buttons

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // JavaScript to toggle the menu
-document.querySelector('.menu-btn').addEventListener('click', function () {
+
+document.addEventListener('DOMContentLoaded', function () {
     const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
+    const menuBtn = document.querySelector('.menu-btn');
+    const menuBtn2 = document.querySelector('.menu-btn-2');
+
+    menuBtn.addEventListener('click', function () {
+        menu.classList.toggle('active');
+        menuBtn.classList.add('hidden'); // Hide menu button
+        menuBtn2.classList.remove('hidden'); // Show close button
+    });
+
+    menuBtn2.addEventListener('click', function () {
+        menu.classList.toggle('active');
+        menuBtn2.classList.add('hidden'); // Hide close button
+        menuBtn.classList.remove('hidden'); // Show menu button
+    });
 });
 
 // Animation
